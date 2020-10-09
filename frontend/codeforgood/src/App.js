548311337aef3e9d5c98c21178e2e3824c6a4992
Jonathan Ma <clma@connect.hku.hk>
2020-10-09 17:27:11 +0800

    Add sample dashboard

--- frontend/codeforgood/src/App.js
+++ frontend/codeforgood/src/App.js
@@ -2,16 +2,16 @@ import React, { useContext } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Login from './pages/Login/Login';
+import PostLogin from './pages/PostLogin/PostLogin';
 import { LoginContext } from './context/LoginContext';
+import Dashboard from "./pages/Dashboard/Dashboard";
 
 function App() {
   const {loginSuccess} = useContext(LoginContext);
   return (
     <div className="App">
-      {!loginSuccess ? 
-      <Login />
-    :
-    <></>}
+      {!loginSuccess ? <Login /> : <Dashboard />}
+      // <PostLogin />}
     </div>
   );
 }
