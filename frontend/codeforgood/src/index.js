94de8b871df7c24412e7f1db84fe83ab5b0af26e
ktleungai <kevinl110689@gmail.com>
2020-10-09 21:12:31 +0800

    fixed UI issue

--- frontend/codeforgood/src/index.js
+++ frontend/codeforgood/src/index.js
@@ -26,7 +26,7 @@ import "./assets/sass/light-bootstrap-dashboard-react.scss?v=1.3.0";
 import "./assets/css/demo.css";
 import "./assets/css/pe-icon-7-stroke.css";
 
-import AdminLayout from "layouts/Admin.jsx";
+import AdminLayout from "./layouts/Admin.jsx";
 
 ReactDOM.render(
   <BrowserRouter>
