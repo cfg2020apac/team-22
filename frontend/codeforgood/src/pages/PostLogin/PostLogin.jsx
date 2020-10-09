5251a7ddad4d4244c3aa080e3433fd9592424d54
Jonathan Ma <clma@connect.hku.hk>
2020-10-09 19:51:01 +0800

    Courses UI

--- frontend/codeforgood/src/pages/PostLogin/PostLogin.jsx
+++ frontend/codeforgood/src/pages/PostLogin/PostLogin.jsx
@@ -4,13 +4,14 @@ import "bootstrap/dist/css/bootstrap.css";
 import "./PostLogin.css";
 import Dashboard from "../Dashboard/Dashboard";
 import NavBar from "../Navbar/NavBar";
+import Courses from "../Courses/Courses";
 
 const PostLogin = props => {
     return (
         <div className="postlogin-page">
             <NavBar/>
             <Dashboard/>
-            <Dashboard/>
+            <Courses/>
         </div>
     );
 };
