aa333f93d6c3270cc96b83ea3b9db02dfd0fc010
ktleungai <kevinl110689@gmail.com>
2020-10-10 08:34:43 +0800

    finalized frontend

--- frontend/codeforgood/src/components/Tasks/Tasks.jsx
+++ frontend/codeforgood/src/components/Tasks/Tasks.jsx
@@ -16,12 +16,12 @@ export class Tasks extends Component {
     const edit = <Tooltip id="edit_tooltip">Edit Task</Tooltip>;
     const remove = <Tooltip id="remove_tooltip">Remove</Tooltip>;
     const tasks_title = [
-      'Sign contract for "What are conference organizers afraid of?"',
-      "Lines From Great Russian Literature? Or E-mails From My Boss?",
-      "Flooded: One year later, assessing what was lost and what was found when a ravaging rain swept through metro Detroi",
-      "Create 4 Invisible User Experiences you Never Knew About",
-      'Read "Following makes Medium better"',
-      "Unfollow 5 enemies from twitter"
+      'Recruit volunteers for JA Finance for Future',
+      "Meet with teachers to explain voluntary details",
+      "Lead the program of JA Job Shadow",
+      "Write articles and summary about JA Innovation Camps",
+      'Prepare materials for JA Company Programme',
+      "Meet with supervisor for JA Hong Kong"
     ];
     var tasks = [];
     var number;
